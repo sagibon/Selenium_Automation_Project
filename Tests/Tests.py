16c30132ab2e4b67ed33283510caca3a77de946d
Dolev <Dolevshoval@icloud.com>
2021-01-26 23:57:12 +0200

    I fixed Test8 & Test9

--- Tests/Tests.py
+++ Tests/Tests.py
@@ -17,6 +17,7 @@ import time
 class AOS_TESTS(unittest.TestCase):
     USERNAME = None
     PASSWORD = None
+
     # a function that adds products to the cart, we use this a lot
     def add_product_to_cart(self, quantity):
         self.product_page.change_quantity(quantity)  # change product quantity
@@ -45,51 +46,51 @@ class AOS_TESTS(unittest.TestCase):
     #     total = self.pop_out.get_total_quantity().text
     #     self.assertEqual(total, '(5 Items)')
     #
-    # def test_2(self):
-    #     self.main_page.click_on_category('tablets')
-    #     self.category_page = Category_Page()  # click on the category page
-    #     self.category_page.scan_products()  # get the list of the products that are in stock from the page
-    #     self.category_page.click_on_product()  # click on some product
-    #     self.product_page1 = Product_Page()  # create instance
-    #     self.product_page1.change_quantity(3)  # change product quantity
-    #     self.product_page1.choose_color()  # choose color
-    #     self.product_page1.add_to_cart()  # adding to cart
-    #     p1 = [self.product_page1.name, self.product_page1.color, self.product_page1.quantity]  # save product parameters
-    #     self.p1_pop_up = Pop_up_checkout()
-    #     p1_pop_up = [self.p1_pop_up.name, self.p1_pop_up.color, self.p1_pop_up.quantity]  # save pop_up parameters of this product
-    #     self.main_page.get_main_page()  # back to main page
-    #     self.main_page.click_on_category('mice')
-    #     self.category_page.scan_products()  # get the list of the products that are in stock from the page
-    #     self.category_page.click_on_product()
-    #     self.product_page2 = Product_Page()
-    #     self.product_page2.change_quantity(5)  # change product quantity
-    #     self.product_page2.choose_color()
-    #     self.product_page2.add_to_cart()
-    #     p2 = [self.product_page2.name, self.product_page2.color, self.product_page2.quantity]
-    #     self.p2_pop_up = Pop_up_checkout()
-    #     p2_pop_up = [self.p2_pop_up.name, self.p2_pop_up.color, self.p2_pop_up.quantity]
-    #     self.main_page.get_main_page()
-    #     self.main_page.click_on_category('headphones')
-    #     self.category_page.scan_products()
-    #     self.category_page.click_on_product()
-    #     self.product_page3 = Product_Page()
-    #     self.product_page3.change_quantity(8)
-    #     self.product_page3.choose_color()
-    #     self.product_page3.add_to_cart()
-    #     self.pop_up_checkout = Pop_up_checkout()
-    #     p3 = [self.product_page3.name, self.product_page3.color, self.product_page3.quantity]
-    #     self.p3_pop_up = Pop_up_checkout()
-    #     p3_pop_up = [self.p3_pop_up.name, self.p3_pop_up.color, self.p3_pop_up.quantity]
-    #     self.assertEqual(p1[1:], p1_pop_up[1:])  # comparing everything but the name, because the pop up name will
-    #     self.assertEqual(p2[1:], p2_pop_up[1:])  # ...be shortened and requires a different assertion test
-    #     self.assertEqual(p3[1:], p3_pop_up[1:])
-    #     # comparing pop up name to name
-    #     self.assertIn(p1_pop_up[0][:-3], p1[0])  # without the '...' in the end - unnecessary
-    #     self.assertIn(p2_pop_up[0][:-3], p2[0])
-    #     self.assertIn(p3_pop_up[0][:-3], p3[0])
-    #     # for p in [p1, p2, p3]:
-    #     #     p_pop_up = [p1_pop_up, p2_pop_up, p3_pop_up]
-    #     #     self.assertIn(p[0], p_pop_up[p.index(p)][0])
+    def test_2(self):
+        self.main_page.click_on_category('tablets')
+        self.category_page = Category_Page()  # click on the category page
+        self.category_page.scan_products()  # get the list of the products that are in stock from the page
+        self.category_page.click_on_product()  # click on some product
+        self.product_page1 = Product_Page()  # create instance
+        self.product_page1.change_quantity(3)  # change product quantity
+        self.product_page1.choose_color()  # choose color
+        self.product_page1.add_to_cart()  # adding to cart
+        p1 = [self.product_page1.name, self.product_page1.color, self.product_page1.quantity]  # save product parameters
+        self.p1_pop_up = Pop_up_checkout()
+        p1_pop_up = [self.p1_pop_up.name, self.p1_pop_up.color, self.p1_pop_up.quantity]  # save pop_up parameters of this product
+        self.main_page.get_main_page()  # back to main page
+        self.main_page.click_on_category('mice')
+        self.category_page.scan_products()  # get the list of the products that are in stock from the page
+        self.category_page.click_on_product()
+        self.product_page2 = Product_Page()
+        self.product_page2.change_quantity(5)  # change product quantity
+        self.product_page2.choose_color()
+        self.product_page2.add_to_cart()
+        p2 = [self.product_page2.name, self.product_page2.color, self.product_page2.quantity]
+        self.p2_pop_up = Pop_up_checkout()
+        p2_pop_up = [self.p2_pop_up.name, self.p2_pop_up.color, self.p2_pop_up.quantity]
+        self.main_page.get_main_page()
+        self.main_page.click_on_category('headphones')
+        self.category_page.scan_products()
+        self.category_page.click_on_product()
+        self.product_page3 = Product_Page()
+        self.product_page3.change_quantity(8)
+        self.product_page3.choose_color()
+        self.product_page3.add_to_cart()
+        self.pop_up_checkout = Pop_up_checkout()
+        p3 = [self.product_page3.name, self.product_page3.color, self.product_page3.quantity]
+        self.p3_pop_up = Pop_up_checkout()
+        p3_pop_up = [self.p3_pop_up.name, self.p3_pop_up.color, self.p3_pop_up.quantity]
+        self.assertEqual(p1[1:], p1_pop_up[1:])  # comparing everything but the name, because the pop up name will
+        self.assertEqual(p2[1:], p2_pop_up[1:])  # ...be shortened and requires a different assertion test
+        self.assertEqual(p3[1:], p3_pop_up[1:])
+        # comparing pop up name to name
+        self.assertIn(p1_pop_up[0][:-3], p1[0])  # without the '...' in the end - unnecessary
+        self.assertIn(p2_pop_up[0][:-3], p2[0])
+        self.assertIn(p3_pop_up[0][:-3], p3[0])
+        # for p in [p1, p2, p3]:
+        #     p_pop_up = [p1_pop_up, p2_pop_up, p3_pop_up]
+        #     self.assertIn(p[0], p_pop_up[p.index(p)][0])
     #
     # def test_3(self):
     #     self.main_page.click_on_category('tablets')
@@ -245,18 +246,18 @@ class AOS_TESTS(unittest.TestCase):
         x = self.main_page.check_user_orders()
         self.assertEqual(product_name, x.upper())
 
-    # def test_10(self):
-    #     username = 'test_xyz'
-    #     password = 'Aasd123'
-    #     # clicks the login icon above from main and logins
-    #     self.main_page.click_to_login_from_main_page(username, password)  # signs in with this credentials
-    #     account_name = self.main_page.check_login_name()
-    #     self.assertEqual(username, account_name)  # sees if the account shown above the account icon is the same
-    #     time.sleep(2)  # wait between login and logout
-    #     self.main_page.logout_user()  # logs out of the account
-    #     time.sleep(1)  # give time to logout safely
-    #     account_name = self.main_page.check_login_name()  # resigns new account name
-    #     self.assertEqual(account_name, 'out')  # if the user is logged out, the check login name method returns "out"
+    def test_10(self):
+        username = AOS_TESTS.USERNAME
+        password = AOS_TESTS.PASSWORD
+        # clicks the login icon above from main and logins
+        self.main_page.click_to_login_from_main_page(username, password)  # signs in with this credentials
+        account_name = self.main_page.check_login_name()
+        self.assertEqual(username, account_name)  # sees if the account shown above the account icon is the same
+        time.sleep(2)  # wait between login and logout
+        self.main_page.logout_user()  # logs out of the account
+        time.sleep(1)  # give time to logout safely
+        account_name = self.main_page.check_login_name()  # resigns new account name
+        self.assertEqual(account_name, 'out')  # if the user is logged out, the check login name method returns "out"
 
     def tearDown(self):
         time.sleep(5)
